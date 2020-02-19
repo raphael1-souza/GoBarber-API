8bf4e4862b5c17c00c6a92b04388a81e6c01ceca
raphael <raphasilvasouza96@gmail.com>
2020-02-18 22:21:20 -0300

    update: update the code

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'teste' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Raphael',
+    email: 'raphael.souza@gmail.com',
+    password_hash: '123455556',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
