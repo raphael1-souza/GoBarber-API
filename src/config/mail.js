48245b7107dd0534ced96f12576dd31b7c11e19d
raphael <raphasilvasouza96@gmail.com>
2020-04-15 22:42:20 -0300

    feat: mail configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '25582e6b2ff88e',
+    pass: '415ae875489a4b',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
